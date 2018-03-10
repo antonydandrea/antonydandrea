f8fef6586ad7a3f86a09e514e077a2301b34fc4f
Antony D'Andrea <antony492@yahoo.co.uk>
2018-03-10 08:42:26 +0000

    added twig bundle and first template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,14 +1,13 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController
+class DefaultController extends Controller
 {
     public function index()
     {
-        return new Response(
-            'Hello, Wolrd'
-        );
+        return $this->render('main.html.twig', []);
     }
 }
